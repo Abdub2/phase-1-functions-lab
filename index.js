21aa15afa26e60c6e371b7a82ad5a5ac308b3175
Abdub2 <abdubdoyo2005@gmail.com>
2022-11-24 16:19:27 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    const hq=42
+    if (distance>hq){
+        return(distance-hq);
+    }else if(distance<hq){
+        return(hq-distance);
+    }
+}
+distanceFromHqInBlocks(13);
+
+function distanceFromHqInFeet(distanceF){
+  return 264 *distanceFromHqInBlocks(distanceF);
+
+}
+distanceFromHqInFeet(43);
+
+function distanceTravelledInFeet(start,destination){
+    if(start>destination){
+        return (start -destination)*264
+    }else {
+        return(destination-start)*264
+    }
+}
+distanceTravelledInFeet(50,58);
+
+function calculatesFarePrice(start,destination){
+    const distance= distanceTravelledInFeet(start,destination);
+    if (distance>2500){
+        return "cannot travel that far";
+    } else if(distance>2000) {
+        return 25;
+    } else if (distance<=400){
+        return 0;
+    }else if(400<distance<2000){
+        return 0.02*(distance -400);
+    }
+
+}
+
+calculatesFarePrice(34,24);
